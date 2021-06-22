44c07cbbcfc3a60e5bd74003d3d00049e8e779e3
Matthew Ballance <matt.ballance@gmail.com>
2021-06-21 22:15:44 -0700

    Remove use of --no-cache flag
    Signed-off-by: Matthew Ballance <matt.ballance@gmail.com>

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ if test $? -ne 0; then exit 1; fi
 docker run -v `pwd`/dv-tools:/dv-tools fw-dv-tools-build /dv-tools/build.sh
 if test $? -ne 0; then exit 1; fi
 
-docker build --no-cache -t fw-dv-tools -v `pwd`/dv-tools:/dv-tools dv-tools
+#docker build --no-cache -t fw-dv-tools -v `pwd`/dv-tools:/dv-tools dv-tools
+docker build -t fw-dv-tools -v `pwd`/dv-tools:/dv-tools dv-tools
 if test $? -ne 0; then exit 1; fi
 
